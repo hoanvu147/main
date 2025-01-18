cdc6449849ce14edaf745576ab2fe6d7cc9419ca
hoanvu147 <91400514+hoanvu147@users.noreply.github.com>
2025-01-18 14:05:13 +0700

    Add files via upload

---
+++ nguoiyeucuaem/config.js
@@ -0,0 +1,22 @@
+const CONFIG = {
+    introTitle: 'Xin Chào',
+    introDesc: `Rất vui khi gặp nàng`,
+    btnIntro: 'Bắt Đầu',
+    title: 'Bạn Làm Người Yêu Mình Nhé!',
+    desc: 'Đừng hòng chạy thoát',
+    btnYes: 'Đồng Ý luôn',
+    btnNo: 'CÚT',
+    question:'BẠN THÍCH MÌNH ĐIỂM NÀO',
+    btnReply: 'XÁC NHẬN',
+    reply: 'TẤT CẢ MỌI THỨ',
+    mess: 'CÁM ƠN Vì ĐÃ CHÁP NHẬN MÌNH',
+    messDesc: 'saranghae',
+    btnAccept: 'KHÔNG CÓ CHI',
+    messLink: 'https://www.facebook.com/katze.katze.714', //link fb
+    bgLink: 'https://scontent.fsgn5-10.fna.fbcdn.net/v/t1.6435-9/104358214_148056190154306_8327379159412351887_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=86c6b0&_nc_ohc=fQFCswlw0QsQ7kNvgGlDB0Z&_nc_zt=23&_nc_ht=scontent.fsgn5-10.fna&_nc_gid=AqyJCykIK-65GU20cpVQSVY&oh=00_AYBJwaFykr7DMA_b0iIStyAdUpxN6T73eslLxjZB9i6mTQ&oe=67B2B455', //link ảnh nền
+    iputBgLink: 'https://recmiennam.com/wp-content/uploads/2020/09/hinh-anh-cap-doi-lang-man-1.jpg', //link iputBgLink
+    lookMeLink: 'https://scontent.fsgn5-3.fna.fbcdn.net/v/t39.30808-6/470206325_1098815021745080_3923732659242131342_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=XwizGcBF-FMQ7kNvgEnBc6e&_nc_zt=23&_nc_ht=scontent.fsgn5-3.fna&_nc_gid=AfhyGwRw6w24-LRfnjlGDR3&oh=00_AYB0ilLr_HzmtqHnGFUxeXFnnk9QN-m1MO49X0fKCXFB7Q&oe=6791249C', //link ảnh lookMe
+    soundLink: 'https://huypham9205.github.io/tang-crush/sound/sound.mp3', //link nhac
+    duckLink: 'https://huypham9205.github.io/tang-crush/sound/duck.mp3', //link aam thanh 1
+    swishLink: 'https://huypham9205.github.io/tang-crush/sound/Swish1.mp3', //link swish
+}
